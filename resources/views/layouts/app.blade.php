3f370920d155be7b2edd34e05f6d2ff197e29c34
ettouktio <ettouktio@gmail.com>
2026-05-11 02:15:34 +0100

    Fix syntax and rebuild assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,8 @@
         })();
     </script>
     <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<script src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>
     <div class="site-shell">
